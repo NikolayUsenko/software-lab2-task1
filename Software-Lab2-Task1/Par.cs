acc2f6e3288fae3c7a667d757b0f2445cf810ad1
Николай <usenkko.nikolay@gmail.com>
2025-10-11 14:29:50 +0300

    Добавьте файлы проекта.

---
+++ Software-Lab2-Task1/Par.cs
@@ -0,0 +1,45 @@
+﻿class Par
+{
+    private double a, b, c;
+    private Par() { }
+    public Par(double a, double b, double c)
+    {
+        this.a = a;
+        this.b = b;
+        this.c = c;
+    }
+    public double GetV() => a * b * c;
+    public double GetS() => 2 * (a * b + b * c + a * c);
+    public void Info()
+    {
+        String str =
+            "\n*******************************\n" +
+            "*                             *\n" +
+            "*       Параллелепипед        *\n" +
+            "*                             *\n" +
+            "*******************************\n";
+        Console.WriteLine(str);
+        Console.WriteLine(string.Format("Значения сторон параллелепипеда:\nA = {0:0.00};\nB = {1:0.00};\nC = {2:0.00}.", a, b, c));
+        Console.WriteLine(string.Format("Объём и площадь поверхности параллелепипеда:\nV = {0:0.00}\nS = {1:0.00}", GetV(), GetS()));
+    }
+    public void Info(ConsoleColor fg, ConsoleColor bgc)
+    {
+        Console.ForegroundColor = fg;
+        Console.BackgroundColor = bgc;
+        Console.Clear();
+        Info();
+    }
+    public void Load()
+    {
+        a = Convert.ToDouble(Console.ReadLine());
+        b = Convert.ToDouble(Console.ReadLine());
+        c = Convert.ToDouble(Console.ReadLine());
+    }
+    public static Par CreateParFromFile()
+    {
+        double A = Convert.ToDouble(Console.ReadLine());
+        double B = Convert.ToDouble(Console.ReadLine());
+        double C = Convert.ToDouble(Console.ReadLine());
+        return new Par(A, B, C);
+    }
+}
---
+++ Software-Lab2-Task1/Program.cs
@@ -0,0 +1,30 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+#if RELEASE
+        TextWriter save_out = Console.Out;
+        TextReader save_in = Console.In;
+        var new_out = new StreamWriter(@"par_output.txt");
+        var new_in = new StreamReader(@"par_input.txt");
+        Console.SetOut(new_out);
+        Console.SetIn(new_in);
+#endif
+        Par p1, p2;
+#if DEBUG
+        p2 = new(5.7, 11, 18);
+        p2.Info(ConsoleColor.Black, ConsoleColor.White);
+#endif
+#if RELEASE
+        p1 = Par.CreateParFromFile();
+        p1.Info();
+        p2 = new(5.6, 5.11, 10.5);
+        p2.Info();
+        Console.SetOut(save_out); new_out.Close();
+        Console.SetIn(save_in); new_in.Close();
+#endif
+#if DEBUG
+        Console.ReadKey();
+#endif
+    }
+}
